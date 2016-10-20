12dd4bb8858b8b9f8ce0faaf8541cf80f4572c78
belen_vldm <belen.vldm@gmail.com>
2016-10-20 10:03:29 -0300

    Bombas y timer

--- js/Menu.js
+++ js/Menu.js
@@ -1,5 +1,7 @@
 var Jueguin = Jueguin || {};
-var button; // Creo la variable para el boton
+var button;         // Creo la variable para el boton
+var txtInstruc;     // Texto de instrucciones
+var txtInstruc2;     // Texto de instrucciones
 
 // Creo el estado del Menu
 Jueguin.MenuState = {
@@ -17,6 +19,10 @@ Jueguin.MenuState = {
     },
 
     create: function () {
+        txtInstruc = this.game.add.text(this.game.world.centerX - 500, this.game.world.centerY - 300, '       Violetin:\n w: up, s: down \n a: left, d: right \n q: bomba, 1: correr', {fontSize: '32px',fill: '#de98ff'});
+        txtInstruc2 = this.game.add.text(this.game.world.centerX + 100, this.game.world.centerY - 300, '             Rositin:\n up_arrow: up, down_arrow: down \n left_arrow: left, right_arrow: right \n {: bomba, -: correr', {fontSize: '32px',fill: '#ff98b1'});
+
+
         // Situo el boton y le setteo el ancla en el medio
         button = this.game.add.button(this.game.world.centerX, this.game.world.centerY + 180, 'button', actionOn, this, 1, 0);
         button.anchor.setTo(0.5);
--- js/states/Preload.js
+++ js/states/Preload.js
@@ -5,11 +5,14 @@ Jueguin.PreloadState = {
         // Cargo las imagenes que voy a usar
         this.load.tilemap('myTilemap', 'assets/tilemap/mapita.json', null, Phaser.Tilemap.TILED_JSON);
         this.load.image('myTileset', 'assets/tilemap/tilealien1.jpg');
-        // this.load.image('background', 'assets/img/background.jpg');
         this.load.spritesheet('playerUno', 'assets/img/player_uno.png', 32, 48);
         this.load.spritesheet('playerDos', 'assets/img/player_dos.png', 32, 48);
-        this.load.image('starUno', 'assets/img/estre.png');
-        this.load.image('starDos', 'assets/img/estre_uno.png');
+        this.load.image('starUno', 'assets/img/estre_uno.png');
+        this.load.image('starDos', 'assets/img/estre.png');
+        this.load.image('bombaUno', 'assets/img/boom_uno.png', 48, 48);
+        this.load.image('bombaDos', 'assets/img/boom.png', 48, 48);
+        this.load.image('explosion', 'assets/img/fire.png');
+        this.load.image('bgWin', 'assets/img/back.png');
     },
 
     create: function() {
